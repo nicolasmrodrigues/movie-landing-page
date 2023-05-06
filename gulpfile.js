13c13242304802c0684cce9c3e12b7f3ddcceb6a
Nícolas Medeiros Rodrigues <nicolasmedeiros090@gmail.com>
2023-05-06 15:40:43 -0300

    adicionando minificação de arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
+'use strict';
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify-es').default;
 
-function compilaSaas() {
-    return gulp.src('src/styles/**/*.scss')
+function compilaSass() {
+    return gulp.src('source/sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({outputStyle: "compressed"}))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('dist/styles/'))
+    .pipe(gulp.dest('build/css'));
+};
+
+function comprimeImagens() {
+    return gulp.src('source/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/images'));
+};
+
+function minificaHTML() {
+    return gulp.src('source/index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
 }
 
-exports.build = gulp.series(compilaSaas)
-exports.dev = function() {
-    gulp.watch('src/styles/**/*.scss', gulp.series(compilaSaas))
+function minificaJS() {
+    return gulp.src('source/javascript/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/javascript/'));
 }
+
+exports.dev = function () {
+    gulp.watch('./source/styles/*.scss', gulp.parallel(compilaSass))
+    gulp.watch('source/scripts/*js', gulp.parallel(minificaJS))
+}
+
+exports.build = gulp.series(compilaSass, comprimeImagens, minificaHTML, minificaJS);
